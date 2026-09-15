35eb9d08fce832c59fa40e7fb43f9be46f0ed8a3
Emma-123-maker <euniceomodelta@gmail.com>
2026-09-15 11:57:48 +0100

    Create HomeScreen widget with basic layout

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('NaijaCopas Home')),
+      body: Center(child: Text('This is the real home page')),
+    );
+  }
+}
