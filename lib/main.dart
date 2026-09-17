4f72e042a4f6a71a6604c48b46e5b26cf3194164
Emma-123-maker <euniceomodelta@gmail.com>
2026-09-17 06:13:53 +0100

    Add Firebase Storage import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
